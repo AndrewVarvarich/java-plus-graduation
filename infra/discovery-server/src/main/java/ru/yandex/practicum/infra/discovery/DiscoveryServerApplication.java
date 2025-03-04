6ceff387866ece2d3570d4320d9d37a689d076eb
Andrew Varvarich <warwarich@yandex.ru>
2025-03-04 20:21:06 +0300

    fix

--- infra/discovery-server/src/main/java/ru/yandex/practicum/infra/discovery/DiscoveryServerApplication.java
+++ infra/discovery-server/src/main/java/ru/yandex/practicum/infra/discovery/DiscoveryServerApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
-public class DiscoveryServer {
-
+@EnableEurekaServer
+public class DiscoveryServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServer.class, args);
+        SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 }
